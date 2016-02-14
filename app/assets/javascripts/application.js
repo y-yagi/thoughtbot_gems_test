b8edbf7415137ddb818693717f30d144086d92c5
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-02-14 13:03:46 +0900

    add `paul_revere` example

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require announcements
 //= require turbolinks
 //= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214021026) do
+ActiveRecord::Schema.define(version: 20160214040107) do
+
+  create_table "announcements", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
